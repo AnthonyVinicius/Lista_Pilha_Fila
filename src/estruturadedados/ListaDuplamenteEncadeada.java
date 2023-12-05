b8f6709967ab4ae72472cefa6267933bd45d6ce7
Anthony Vinicius <anthonyvinicius1312@gmail.com>
2023-12-04 23:37:43 -0300

    m

--- src/estruturadedados/ListaDuplamenteEncadeada.java
+++ src/estruturadedados/ListaDuplamenteEncadeada.java
@@ -37,6 +37,7 @@ public class ListaDuplamenteEncadeada {
 			}
 			else {
 				novoNo.setProximoNo(primeiroNo);
+				primeiroNo.setProximoNo(novoNo);
 				primeiroNo = novoNo;
 				System.out.println("2");
 			}
